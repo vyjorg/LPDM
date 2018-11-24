98db6a0093ad5c870fde9f5faaa83763ecc20085
Kybox <nslr@riseup.net>
2018-11-24 20:51:46 +0100

    Adding the project architecture

---
+++ src/ms-order/src/main/java/com/lpdm/msorder/MsOrderApplication.java
@@ -0,0 +1,12 @@
+package com.lpdm.msorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsOrderApplication.class, args);
+	}
+}
