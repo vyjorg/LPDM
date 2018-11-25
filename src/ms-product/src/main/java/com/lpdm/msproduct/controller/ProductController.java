1221cc7ea881f2ea22bde656b70c22290262f522
vianney diris <vianney.diris@gmail.com>
2018-11-25 18:34:39 +0100

    ajouts de méthodes listProduct et findProduct

--- src/ms-product/src/main/java/com/lpdm/msproduct/controller/ProductController.java
+++ src/ms-product/src/main/java/com/lpdm/msproduct/controller/ProductController.java
@@ -1,12 +1,29 @@
 package com.lpdm.msproduct.controller;
 
 import com.lpdm.msproduct.dao.ProductDao;
+import com.lpdm.msproduct.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
 
     @Autowired
     private ProductDao productDao;
+
+    @GetMapping(value = "/Products")
+    public List<Product> listProduct(){
+        List<Product> list = productDao.findAll();
+
+        return list;
+    }
+
+    @GetMapping(value="/Product/{id}")
+    public Product findProduct(@PathVariable int id){
+        Product product = productDao.findById(id);
+
+        return product;
+    }
 }
--- src/ms-product/src/main/java/com/lpdm/msproduct/dao/ProductDao.java
+++ src/ms-product/src/main/java/com/lpdm/msproduct/dao/ProductDao.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductDao extends JpaRepository<Product, Integer>  {
+
+    Product findById(int id);
+
 }
