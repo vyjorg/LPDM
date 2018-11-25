d7bee8ffb33c460bb7823a4eaafa93dbd3afd491
Kybox <nslr@riseup.net>
2018-11-25 17:15:33 +0100

    (ms-order) Add SpringCloudConfig Client
    For test

---
+++ src/ms-order/src/main/java/com/lpdm/msorder/controller/CloudConfigController.java
@@ -0,0 +1,22 @@
+package com.lpdm.msorder.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class CloudConfigController {
+
+    @Value("${msorder.fullname:Un nom lambda}")
+    private String fullName;
+
+    @ResponseBody
+    @RequestMapping("/showInfos")
+    public String showInfos(){
+
+        return "Microservice : " + fullName;
+    }
+}
