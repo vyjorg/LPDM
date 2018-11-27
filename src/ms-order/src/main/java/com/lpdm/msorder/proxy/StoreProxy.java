f21195d809c9c98a5843d3303f2e453e308aafe8
Kybox <nslr@riseup.net>
2018-11-27 18:52:54 +0100

    (ms-order) Microservice update
    - Updating FeignClient data in proxies
    - Updating OrderController to get user obejct by id
    - Adding UserController

--- src/ms-order/src/main/java/com/lpdm/msorder/proxy/StoreProxy.java
+++ src/ms-order/src/main/java/com/lpdm/msorder/proxy/StoreProxy.java
@@ -2,16 +2,18 @@ package com.lpdm.msorder.proxy;
 
 import com.lpdm.msorder.entity.Store;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Component
-@FeignClient(name = "ms-store", url = "localhost:28084")
+@FeignClient(name = "${microservice.store.name}",
+        url = "${microservice.domain}"+":"+"${microservice.store.port}")
 public interface StoreProxy {
 
     @RequestMapping(value = "/stores/{id}",
-            method = RequestMethod.GET, consumes = "application/json")
+            method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     Store findById(@PathVariable(value = "id") int id);
 }
