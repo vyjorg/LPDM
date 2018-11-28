366ae068cab60b2a07f1f992740fe3093a8c3ba9
julien <juliencauwet@yahoo.fr>
2018-11-28 07:55:38 -0600

    added UserController methods in msuser + handling UserNotFoundException

--- src/ms-user/src/main/java/com/lpdm/msuser/web/controllers/UserController.java
+++ src/ms-user/src/main/java/com/lpdm/msuser/web/controllers/UserController.java
@@ -2,11 +2,13 @@ package com.lpdm.msuser.web.controllers;
 
 import com.lpdm.msuser.beans.AppUser;
 import com.lpdm.msuser.dao.AppUserRepository;
+import com.lpdm.msuser.web.exceptions.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -19,4 +21,23 @@ public class UserController {
         return appUserRepository.findAll();
     }
 
+    @GetMapping("/users/{id}")
+    public Optional<AppUser> getUserById(@PathVariable int id){
+
+        Optional <AppUser> appUser = appUserRepository.findById(id);
+        if(!appUser.isPresent())
+            throw new UserNotFoundException("Could not find any user matching this id " + id);
+
+        return appUser;
+    }
+
+    @PostMapping("/users")
+    public void addUser(AppUser user){
+        appUserRepository.save(user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id){
+        appUserRepository.deleteById(id);
+    }
 }
